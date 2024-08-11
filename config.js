4187e1af7966cebf72fdd9350359877cffb5e063
vijay <153777525+Vijay-kumar-das@users.noreply.github.com>
2024-08-12 03:52:17 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+    congnito: {
+        userPoolId: 'us-west-2_4YaxezEry',
+        userPoolClientId: '6ma2p7ggi2ackma921gc9bj7rh',
+        region: 'us-west-2'
+    },
+    api: {
+        invokeUrl: '',
+    }
+};
